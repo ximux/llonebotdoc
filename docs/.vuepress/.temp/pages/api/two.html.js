83fd3b91282e33767037605596de206984e83e74
ximux <60070136+ximux@users.noreply.github.com>
2024-09-11 16:26:44 +0800

    Delete docs/.vuepress/.temp directory

--- docs/.vuepress/.temp/pages/api/two.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "C:/publicfile/llonebotdoc/docs/.vuepress/.temp/pages/api/two.html.vue"
-const data = JSON.parse("{\"path\":\"/api/two.html\",\"title\":\"api2\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1725963825000,\"contributors\":[{\"name\":\"ximu\",\"email\":\"2282354201@qq.ocm\",\"commits\":2},{\"name\":\"ximux\",\"email\":\"60070136+ximux@users.noreply.github.com\",\"commits\":1}]},\"filePathRelative\":\"api/two.md\"}")
-export { comp, data }
--- docs/.vuepress/.temp/prismjs/config.js
+++
@@ -1,3 +0,0 @@
-import "C:/publicfile/llonebotdoc/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "C:/publicfile/llonebotdoc/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "C:/publicfile/llonebotdoc/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
